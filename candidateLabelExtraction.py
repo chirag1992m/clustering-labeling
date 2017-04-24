02da965a747de7bd7fef47fcf6dc9d7cb004c5da
Chirag Maheshwari <chirag1992m.cc@gmail.com>
2017-04-24 18:18:09 -0400

    add empty files for modulation

---
+++ candidateLabelExtraction.py
@@ -0,0 +1,7 @@
+'''
+Course: Search Engine Architecture
+Project: Cluster-Labeling
+
+File: candidateLabelExtraction.py
+Job: Extracting representative labels of clusters
+'''
---
+++ cluster.py
@@ -0,0 +1,7 @@
+'''
+Course: Search Engine Architecture
+Project: Cluster-Labeling
+
+File: cluster.py
+Job: Contains clustering methods
+'''
