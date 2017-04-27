39f461113ec8164bed95648d4e211b1ffe54e61a
Chirag Maheshwari <chirag1992m.cc@gmail.com>
2017-04-27 19:30:04 -0400

    Fixing wikiSearch with currrent stucture of inputs/outputs

--- wikiSearch.py
+++ wikiSearch.py
@@ -1,32 +1,41 @@
 import wikipedia
 import pickle
+import inventory
 
+def calc_score(idx):
+    return (inventory.MAX_DOCS - idx)/(inventory.MAX_DOCS + 1)
+
+def extractLabels(input_terms):
+    output_terms = []
+    for term in input_terms:
+        print(term)
+        scores = []
+        doc_list = wikipedia.search(term)
+        if doc_list == []:
+            continue
+        for idx, doc in enumerate(doc_list[:inventory.MAX_DOCS]):
+            try:
+                doc_page = wikipedia.page(doc)
+                title = doc_page.title
+                categories = doc_page.categories
+                labels = [(title, calc_score(idx))].extend([(cat, calc_score(idx)) for cat in categories if len(cat.split()) < 5])
+                output_terms.append(labels)
+            # check this error
+            except wikipedia.exceptions.DisambiguationError:
+                pass
+            except wikipedia.exceptions.PageError:
+                pass
+    return output_terms
+
+imp_terms = pickle.load(open('important_words.pkl', 'rb'))
+
+for i in range(inventory.NUM_CLUSTERS):
+    top_imp_terms = [x[0] for x in imp_terms[i][:inventory.NUM_TOP_WORDS]]
+    print(top_imp_terms)
+    pickle.dump(extractLabels(top_imp_terms), open('labels_words_{}.pkl'.format(i), 'wb'))
 
 '''
 TODO: Only the first title of the term was taken into consideration.
 All titles used currently.
 All categories of the page are added.
 '''
-def extractLabels(input_file, output_file):
-    with open(input_file) as f:
-        imp_terms = pickle.load(f)
-    f = open(output_file, "wb")
-    for term in imp_terms:
-        new_terms = []
-        doc_list = wikipedia.search(term)
-        if doc_list == []:
-            continue
-        for doc in doc_list:
-            try:
-                doc_page = wikipedia.page(doc)
-                title = doc_page.title
-                categories = doc_page.categories
-                new_terms.append(title)
-                new_terms.extend(categories)
-                for nterm in new_terms:
-                    f.write(nterm + "\n")
-                # f.write("\n")
-            # check this error
-            except wikipedia.exceptions.DisambiguationError:
-                pass
-    f.close()
