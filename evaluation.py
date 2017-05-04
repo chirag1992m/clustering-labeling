818a4a610898e1dba98f3e614265e100e554107b
Chirag Maheshwari <chirag1992m.cc@gmail.com>
2017-05-04 10:49:48 -0400

    Score calculation for evaluation

--- evaluation.py
+++ evaluation.py
@@ -2,38 +2,40 @@ import pickle
 import gensim
 import os
 import operator
+import argparse
 
 model = gensim.models.KeyedVectors.load_word2vec_format(
     'GoogleNews-vectors-negative300.bin', binary=True)
 
-def accuracy(true_labels, pred_labels, K):
-    label_list = []
-    for label in true_labels:
-        label_list.append(label.split('.'))
+def accuracy(true_labels, pred_labels):
+    score = 0
 
-    for pred_list in pred_labels:
-        clust_list = []
-        for true_list in label_list:
-            for pred_string in pred_list:
-                clust_sum = 0
-                pred_strings = pred_string.split()
-                for pred_label in pred_strings:
-                    for true_label in true_list:
-                        try:
-                            clust_sum += model.similarity(true_label, pred_label)
-                        except KeyError:
-                            pass
-                clust_list.append(([pred_string, '.'.join(true_list)], clust_sum))
-        clust_sum_sorted = sorted(clust_list, key=operator.itemgetter(1), reverse=True)
-        print(clust_sum_sorted[:K])
+    for tl in true_labels:
+        max_score = 0
+        for pred_label in pred_labels:
+            pred_label = pred_label.split()
+            for pl in pred_label:
+                try:
+                    current_score = model.similarity(tl, pl)
+                    if current_score > max_score:
+                        max_score = current_score
+                except KeyError:
+                    continue
+        score += max_score
+
+    return score
 
 if __name__ == "__main__":
-    true_labels = pickle.load(open(os.path.join(options.intermediate_out_directory, 'unique_labels.pkl'), 'rb'))
-    important_terms = pickle.load(open(os.path.join(options.intermediate_out_directory, 'important_words.pkl'), 'rb'))
-    pred_labels = []
-    for idx, f in enumerate(os.listdir(options.out_directory)):
-        df = os.path.join(options.out_directory, f)
-        pred_labels.append(pickle.load(open(df, 'rb')))
-        pred_labels[idx].extend([t[0] for t in important_terms[idx]])
+    true_labels = ["Computers", "Sports", "Politics", "Religion", "miscellaneous"]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('in_directory', 
+        help="Directory containing evaluated labels", 
+        default="evaluations", type=str)
+    options = parser.parse_args()
 
-    accuracy(true_labels, pred_labels, 5)
+    pred_labels = []
+    for f in os.listdir(options.in_directory):
+        df = os.path.join(options.in_directory, f)
+        pred_labels.extend([x.strip() for x in open(df, 'r').readlines()])
+
+    print("Accuracy: {}".format(accuracy(true_labels, pred_labels)))
